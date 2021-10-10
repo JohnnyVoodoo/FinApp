6bbaa525d79dee7f4def3228ce3e914c492782ef
Vitaly Shein <johnnyvoodoo@live.ru>
2021-10-10 16:22:13 +0700

    ru lang files

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был изменен!',
+    'sent' => 'Мы отправили инструкцию по восстановлению пароля на Ваш электронный адрес!',
+    'throttled' => 'Пожалуйста, подождите, прежде чем повторить попытку.',
+    'token' => 'Не корректный токен изменения пароля.',
+    'user' => "Мы не смогли найти пользователя с таким электронным адресом.",
+];
